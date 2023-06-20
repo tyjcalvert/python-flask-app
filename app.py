6d6cd059cd1c12e044883f4e18bc232e60a46ac6
Tyler Calvert <tylercalvert@Tylers-MacBook-Pro.local>
2023-06-20 11:17:38 -0500

    added json endpoint

--- app.py
+++ app.py
@@ -24,11 +24,30 @@ def greet(name):
     return '<h1>Hello, %s!</h1>' % name
 
 
-# custom flask cli command
-@app.cli.command()
-def hello():
-    """Just say hello."""
-    click.echo('Hello, Human!')
+@app.route('/json')
+def json_route():
+    return {
+        "glossary": {
+            "title": "example glossary",
+            "GlossDiv": {
+                "title": "S",
+                "GlossList": {
+                    "GlossEntry": {
+                        "ID": "SGML",
+                        "SortAs": "SGML",
+                        "GlossTerm": "Standard Generalized Markup Language",
+                        "Acronym": "SGML",
+                        "Abbrev": "ISO 8879:1986",
+                        "GlossDef": {
+                            "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                            "GlossSeeAlso": ["GML", "XML"]
+                        },
+                        "GlossSee": "markup"
+                    }
+                }
+            }
+        }
+    }
 
 
 if __name__ == "__main__":
